af795d418016cd21f1866a2109960787db9263b4
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-05-17 19:54:08 +0100

    made api accessible from frontend

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 
 const PORT = 8000;
+app.use(cors());
+app.all("*", (request: Request, response: Response) => {
+    response.status(200).send(`A ${request.method} request was made to ${request.protocol}://${request.hostname + ":" + PORT + request.originalUrl}`);
+})
 
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`)
