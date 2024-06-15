f770985eedb302b06f0a7b091d6b53f0de55caac
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-06-15 14:44:38 +0100

    feat: Add createMovie endpoint and update movie routes

---
+++ src/handlers/movies/createMovie.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import prismadb from "../../lib/prismadb";
+import { Movie } from "@prisma/client";
+
+const createMovie = async(request: Request<{}, {}, Movie>, response: Response)=>{
+    const movie = request.body;
+    await prismadb.movie.create({ data: movie });
+    response.status(201).send(movie);
+}
+
+export default createMovie;
